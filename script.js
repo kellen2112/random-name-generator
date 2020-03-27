16645b96992bec902d462534562c02d6e336d1fd
Kellen Sims <kellen2112@gmail.com>
2020-03-26 21:03:05 -0600

    fixed adding last name to js. also worked on making color of female pink but didnt work

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 var btn = document.getElementById('gen-name');
 var currNameContainer = document.getElementById('curr-name');
 
-btn.addEventListener('click', function(){
-    var genderType = getGenderType();
-    var allGender = genderType === 'male' ? maleNameList : femaleNameList;
-    var randomGender = getRandElem(allGender);
-    currNameContainer.innerText = randomGender;
+btn.addEventListener('click', handleClick)
+    
+currNameContainer.addEventListener('change',function(){
+    var color = updateNameColor();
+    document.currNameContainer.style.color = color;
 });
 
-function getGenderType() {
+function handleClick(){
+    var genderType = getGenderType();
+    var validNames = genderType === 'male' ? maleNameList : femaleNameList;
+    var firstName = getRandElem(validNames);
+    var middleName = getRandFilteredElem(validNames, firstName);
+    var lastName = document.getElementById('last-name').value;
+    return currNameContainer.innerText = firstName + ' ' + middleName + ' ' + lastName;
+} 
 
+function enterLastName() {
+    return document.getElementById('last-name').innerText;
+  }
+
+function getGenderType() {
     return document.querySelector('input[type="radio"]:checked').value;
 }
 
@@ -19,4 +31,21 @@ function getRandElem(arr) {
 
 function genRandNum(min, max) {
     return Math.floor(Math.random() * (max + 1 - min) + min);
+}
+
+function getRandFilteredElem(arr, filter){
+    var randEl = getRandElem(arr);
+    while(randEl === filter) {
+       randEl = getRandElem(arr);
+    }
+    return randEl;
+}
+
+function updateNameColor() {
+    if(femaleNameList.checked){
+        return "magenta";
+    } else {
+        return "royalblue"
+    }
+    
 }
